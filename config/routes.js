4fd58025e1b1af0d158619b258ef5ea0aa7b4b9e
msyesyan <msyesyan@gmail.com>
2016-12-23 17:35:52 +0800

    use native http to familiar with node

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,46 @@
-const ThingsController = require('../controllers/things_controller');
+// const ThingsController = require('../controllers/things_controller');
 
-module.exports = (app) => {
-  app.get('/things', ThingsController.index);
-  app.post('/things', ThingsController.create);
-  app.put('/things/:id', ThingsController.update);
-  app.del('/things/:id', ThingsController.destroy);
+// module.exports = (app) => {
+  // eval(require("pryjs").it);
+  // app.on('request', (req, res) => {
+  //   eval(require("pryjs").it);
+  // })
+  // app.request({
+  //   method: 'get',
+  //   path: '/things'
+  // }, ThingsController.index);
+  // app.request({
+  //   method: 'post',
+  //   path: '/things'
+  // }, ThingsController.create);
+  // app.request({
+  //   method: 'patch',
+  //   path: '/things/:id'
+  // }, ThingsController.update);
+  // app.request({
+  //   method: 'post',
+  //   path: '/things'
+  // }, ThingsController.create);
+  // app.request({
+  //   method: 'delete',
+  //   path: 'things/:id'
+  // }, ThingsController.destroy);
+// };
+
+const Routes = (req, res) => {
+  const reqUrl = req.url;
+  // const method = req.method;
+  // eval(require("pryjs").it)
+
+  switch (reqUrl) {
+  case /things/.test(reqUrl) :
+    console.log('true');
+    res.end('things');
+    break;
+  default:
+    console.log('default');
+    res.end('welcome');
+  }
 };
+
+module.exports = Routes;
--- server.js
+++ server.js
@@ -1,25 +1,23 @@
 const ENV = process.env.NODE_ENV;
 
 const path = require('path');
-const restify = require('restify');
+const http = require('http');
 
 const configuration = Object.assign(
   require(path.resolve('./config/application.js')),
   require(path.resolve(`./config/${ENV}.js`))
 );
 
-const server = restify.createServer(configuration);
+const router = require('./config/routes.js');
 
-server.use(restify.acceptParser(server.acceptable));
-server.use(restify.queryParser());
-server.use(restify.bodyParser());
+const server = http.createServer(router);
 
 require(path.resolve('./database.js'))(ENV);
 
-require(path.resolve('./config/routes.js'))(server);
 
-const port = configuration.port || 3000;
-server.listen(port, () => {
+const port = configuration.port;
+server.listen(port, configuration.hostname, (...args) => {
+  console.log(args.length, args);
   console.log(`server startup, listening ${port}`);
 });
 
